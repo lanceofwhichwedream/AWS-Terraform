30ab552ca91af8e18bde9702a15b59fb3e52d9fe
Lance Zeligman <lance.zeligman@gmail.com>
2025-10-30 12:06:23 -0400

    quoting tcp in security rules

--- vpc.tf
+++ vpc.tf
@@ -18,7 +18,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
   security_group_id = aws_security_group.security_network.id
   cidr_ipv4         = aws_vpc.cool_network.cidr_block
   from_port         = 80
-  ip_protocol       = tcp
+  ip_protocol       = "tcp"
   to_port           = 80
 }
 
@@ -26,7 +26,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv4" {
   security_group_id = aws_security_group.security_network.id
   cidr_ipv4         = aws_vpc.cool_network.cidr_block
   from_port         = 443
-  ip_protocol       = tcp
+  ip_protocol       = "tcp"
   to_port           = 443
 }
 
@@ -34,7 +34,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   security_group_id = aws_security_group.security_network.id
   cidr_ipv4         = aws_vpc.cool_network.cidr_block
   from_port         = 22
-  ip_protocol       = tcp
+  ip_protocol       = "tcp"
   to_port           = 22
 }
 
@@ -42,7 +42,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_http_ipv4" {
   security_group_id = aws_security_group.security_network.id
   cidr_ipv4         = aws_vpc.cool_network.cidr_block
   from_port         = 80
-  ip_protocol       = tcp
+  ip_protocol       = "tcp"
   to_port           = 80
 }
 
@@ -50,7 +50,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_https_ipv4" {
   security_group_id = aws_security_group.security_network.id
   cidr_ipv4         = aws_vpc.cool_network.cidr_block
   from_port         = 443
-  ip_protocol       = tcp
+  ip_protocol       = "tcp"
   to_port           = 443
 }
 
@@ -59,7 +59,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv6" {
   security_group_id = aws_security_group.security_network.id
   cidr_ipv6         = aws_vpc.cool_network.ipv6_cidr_block
   from_port         = 80
-  ip_protocol       = tcp
+  ip_protocol       = "tcp"
   to_port           = 80
 }
 
@@ -67,7 +67,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv6" {
   security_group_id = aws_security_group.security_network.id
   cidr_ipv6         = aws_vpc.cool_network.ipv6_cidr_block
   from_port         = 443
-  ip_protocol       = tcp
+  ip_protocol       = "tcp"
   to_port           = 443
 }
 
@@ -75,7 +75,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv6" {
   security_group_id = aws_security_group.security_network.id
   cidr_ipv6         = aws_vpc.cool_network.ipv6_cidr_block
   from_port         = 22
-  ip_protocol       = tcp
+  ip_protocol       = "tcp"
   to_port           = 22
 }
 
@@ -83,7 +83,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_http_ipv6" {
   security_group_id = aws_security_group.security_network.id
   cidr_ipv6         = aws_vpc.cool_network.ipv6_cidr_block
   from_port         = 80
-  ip_protocol       = tcp
+  ip_protocol       = "tcp"
   to_port           = 80
 }
 
@@ -91,6 +91,6 @@ resource "aws_vpc_security_group_egress_rule" "allow_https_ipv6" {
   security_group_id = aws_security_group.security_network.id
   cidr_ipv6         = aws_vpc.cool_network.ipv6_cidr_block
   from_port         = 443
-  ip_protocol       = tcp
+  ip_protocol       = "tcp"
   to_port           = 443
 }
