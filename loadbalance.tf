6d4931fb548c03b50a2c8dd5eef107206493db7a
Lance Zeligman <lance.zeligman@gmail.com>
2025-10-30 23:26:23 -0400

    adding the loadbalancer

---
+++ loadbalance.tf
@@ -0,0 +1,29 @@
+resource "aws_lb" "cool_loadbalancer" {
+  name = "cool-loadbalancer-${var.env}"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.id]
+  subnets = aws_vpc.cool_network.id
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.id
+    prefix  = "cool-lb-${var.env}"
+    enabled = true
+  }
+
+  tags = {
+    Environment = var.env
+  }
+
+}
+
+resource "aws_lb_listener" "cool_alb_listener" {
+  load_balancer_arn = aws_lb.cool_loadbalancer.arn
+  port = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
